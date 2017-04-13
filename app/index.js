bfa45d9797067ad4a35260cd541835b06c299296
Bruce Kellerman <brucek17@hotmail.com>
2017-04-12 23:36:15 -0400

    worked on layout and keeping tetris width/height ratio in sync

--- app/index.js
+++ app/index.js
@@ -1,3 +1,24 @@
-/* global document */
+/* global document,window */
 
 import './main.scss';
+
+const canvas = document.getElementById('canvas');
+
+function actualResizeHandler() {
+  let { width } = document.defaultView.getComputedStyle(canvas);
+  width = Number(width.slice(0, width.length - 2));
+  canvas.style.height = `${width * 2}px`;
+}
+
+let resizeTimeout;
+function resizeThrottler() {
+  if (!resizeTimeout) {
+    resizeTimeout = setTimeout(() => {
+      resizeTimeout = null;
+      actualResizeHandler();
+    }, 66); // 15 fps
+  }
+}
+
+window.addEventListener('resize', resizeThrottler, false);
+actualResizeHandler(); // fire on init
